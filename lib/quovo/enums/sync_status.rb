646110942493c3af0d6057e705e1bce2c4dbd298
Devin Moss <devinsmoss@gmail.com>
2018-10-27 22:11:04 -0500

    Add list of terminal statuses for connection syncs

--- lib/quovo/enums/sync_status.rb
+++ lib/quovo/enums/sync_status.rb
@@ -14,6 +14,29 @@ module Quovo
       define :INSTITUTION_UNAVAILABLE, 'institution_unavailable'
       define :SYNCING, 'syncing'
       define :DELETED, 'deleted'
+
+      # INFO:
+      # good  The connection was properly synced.
+      # incorrect_credentials The login credentials for the connection are incorrect.
+      # challenges  There are additional MFA challenges that need to be answered.
+      # user_config The institution requires the end user to log into the connection and resolve an issue. These instructions are institution-specific, and can be found in config_instructions on a connection object.
+      # resync  The connection needs to be resynced to complete the sync process, usually because a time-sensitive challenge has expired.
+      # postponed The institution is inaccessible at the moment. Quovo will attempt another sync at the end of the day.
+      # maintenance There were Quovo-side issues while syncing the connection.
+      # no_accounts There were no accounts found within the connection.
+      # institution_unavailable Quovo is temporarily unable to sync any connections at this institution.
+      # (null value)  The connection was created, but no sync attempt has been completed.
+
+      TERMINAL_STATUSES = [
+        GOOD,
+        INCORRECT_CREDENTIALS,
+        CHALLENGES,
+        USER_CONFIG,
+        RESYNC,
+        MAINTENANCE,
+        NO_ACCOUNTS,
+        INSTITUTION_UNAVAILABLE
+      ].freeze
     end
   end
 end
